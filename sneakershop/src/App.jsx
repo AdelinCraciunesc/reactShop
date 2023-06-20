e346e1b0edb9d6e9394801e39e3e25334ef55788
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2023-06-20 19:55:48 +0300

    home tab changes

--- sneakershop/src/App.jsx
+++ sneakershop/src/App.jsx
@@ -4,35 +4,57 @@ import Navbar from "./components/Navbar/Navbar";
 import NavbarTop from "./components/NavbarTop/NavbarTop";
 import FeaturedSneakers from "./components/FeaturedSneakers/FeaturedSneakers";
 import Categories from "./components/Categories/Categories";
+import HotSneakers from "./components/HotSneakers/HotSneakers";
 
 const App = () => {
-  const images = [
-    "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=826&t=st=1687009047~exp=1687009647~hmac=b606b7f29c8d13c42120282b44098ae7981d0616df9583d98f0896f509e11d15",
-    "https://img.freepik.com/free-photo/cool-canvas-sneakers-men-s-apparel-summer-fashion-photoshoot_53876-105542.jpg?w=826&t=st=1687009073~exp=1687009673~hmac=72177e1139e3e61a66e532e217197614fe2766ad3cf5712a5a5ce69b8b885e03",
-    "https://img.freepik.com/free-photo/female-walking-park-high-quality-photo_114579-92105.jpg?w=826&t=st=1687009100~exp=1687009700~hmac=c35ff4f4b864cb785ca64cc6c4ffe891983d1ed3e5f6255df7ba3159e9beb558",
-  ];
   const SliderImages = [
-    // {
-    //   image:"https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=826&t=st=1687009047~exp=1687009647~hmac=b606b7f29c8d13c42120282b44098ae7981d0616df9583d98f0896f509e11d15"
-    // },
-    // {
-    //   image:"https://img.freepik.com/free-photo/cool-canvas-sneakers-men-s-apparel-summer-fashion-photoshoot_53876-105542.jpg?w=826&t=st=1687009073~exp=1687009673~hmac=72177e1139e3e61a66e532e217197614fe2766ad3cf5712a5a5ce69b8b885e03"
-    // },
-    // {
-    //   image:"https://img.freepik.com/free-photo/female-walking-park-high-quality-photo_114579-92105.jpg?w=826&t=st=1687009100~exp=1687009700~hmac=c35ff4f4b864cb785ca64cc6c4ffe891983d1ed3e5f6255df7ba3159e9beb558"
-    // },
     {
-      image:"https://ethletic.com/wp-content/uploads/2023/03/Ethletics_Twirl_Homepage_Jesse1-1536x1024.png"
+      image:"https://i.imgur.com/vuQoEza.jpg"
     },
     {
-      image:"https://ethletic.com/wp-content/uploads/2023/03/Ethletics_Twirl_Homepage_Jesse1-1536x1024.png"
+      image:"https://i.imgur.com/5lFFjqk.jpg"
     },
     {
-      image:"https://ethletic.com/wp-content/uploads/2023/03/Ethletics_Twirl_Homepage_Jesse1-1536x1024.png"
+      image:"https://i.imgur.com/5J8t6Ul.jpg"
     }
   ];
+  const Sneakers = [
+      {
+        name:"Adidas",
+        Model: "Adidas Yeezy",
+        image:"https://sizeer.ro/media/cache/gallery/rc/eh0k21eq/adidas-treziod-2-barbati-sneakers-verde-gy0045.jpg",
+        size: [41,42,43],
+        price: 200,
+        brand: "https://www.freepnglogos.com/uploads/adidas-logo-png-black-0.png"
+      },
+      {
+        name:"Nike",
+        Model: "Nike Air Jordan",
+        image:"https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-shoes-HMVqph.png",
+        size: [41,42,43],
+        price: 200,
+        brand: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/200px-Jumpman_logo.svg.png"
+      },
+      {
+        name:"Nike",
+        Model: "Nike Air Jordan",
+        image:"https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-shoes-HMVqph.png",
+        size: [41,42,43],
+        price: 200,
+        brand: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/200px-Jumpman_logo.svg.png"
+      },
+      {
+        name:"Nike",
+        Model: "Nike Air Jordan",
+        image:"https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-shoes-HMVqph.png",
+        size: [41,42,43],
+        price: 200,
+        brand: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/200px-Jumpman_logo.svg.png"
+      },
+  ]
+
   const delay = 4000;
-  const text = ["New Arrivals", "Best Sellers", "Hot Sales!"];
+  const text = ["New Arrivals", "Best Sellers", "Hot Sales!", "Never get out of fashion"];
 
   return (
     <div>
@@ -42,6 +64,7 @@ const App = () => {
         <FeaturedSneakers images={SliderImages} delay={delay} text={text} />
       </div>
       <Categories />
+      <HotSneakers sneakers={Sneakers}/>
     </div>
   );
 };

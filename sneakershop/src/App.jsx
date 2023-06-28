045b64109349e3ff0346eadbcda58a4dcc150aab
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2023-06-28 12:49:12 +0300

    Added dropdown list to cart button

--- sneakershop/src/App.jsx
+++ sneakershop/src/App.jsx
@@ -8,18 +8,59 @@ import Home from "./pages/Home";
 
 const App = () => {
   const [cartItems, setCartItems] = React.useState(0);
+  const firstThreeSneakers = [
+    {
+      name: "Abibas",
+      Model: "YeezyV1",
+      image:
+        "https://sneakerindustry.ro/100093-product_cover/x-psg-air-200e.jpg",
+      size: ["41, 42"],
+      price: 120,
+      brand: "https://www.freepnglogos.com/uploads/adidas-logo-png-black-0.png",
+    },
+    {
+      name: "Nikecss",
+      Model: "Voosh",
+      image:
+        "https://sneakerindustry.ro/102393-product_cover/slipstream-always-on.jpg",
+      size: ["40, 42, 45"],
+      price: 80,
+      brand:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/200px-Jumpman_logo.svg.png",
+    },
+    {
+      name: "Filandro",
+      Model: "Armonica",
+      image: "https://sneakerindustry.ro/102839-product_cover/oznova.jpg",
+      size: ["41, 42, 43"],
+      price: 175,
+      brand:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/200px-Jumpman_logo.svg.png",
+    }
+  ];
 
   return (
     <div>
       <NavbarTop />
-      <Navbar cartItems={cartItems} setCartItems={setCartItems}
+      <Navbar
+        firstThreeSneakers={firstThreeSneakers}
+        cartItems={cartItems}
+        setCartItems={setCartItems}
       />
       {/* <FeaturedSneakers images={SliderImages} delay={delay} text={text}/>
       <Categories />
       <HotSneakers cartItems={cartItems} setCartItems={setCartItems} sneakers={Sneakers} /> */}
       <Routes>
-        <Route path="/" element={<Home cartItems={cartItems} setCartItems={setCartItems} />} />
-        <Route path="/cart" element={<ShoppingCart cartItems={cartItems} setCartItems={setCartItems} />} />
+        <Route
+          path="/"
+          element={<Home cartItems={cartItems} setCartItems={setCartItems} />}
+        />
+        <Route
+          path="/cart"
+          element={
+            <ShoppingCart cartItems={cartItems} setCartItems={setCartItems} />
+          }
+        />
       </Routes>
     </div>
   );
--- sneakershop/src/components/Navbar/Navbar.jsx
+++ sneakershop/src/components/Navbar/Navbar.jsx
@@ -1,155 +1,184 @@
-import * as React from 'react';
-import './Navbar.css'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import { ShoppingCart } from '@mui/icons-material';
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
-import Badge from '@mui/material/Badge';
-import { Link, useHistory } from "react-router-dom";
+import * as React from "react";
+import "./Navbar.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { ShoppingCart } from "@mui/icons-material";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import Badge from "@mui/material/Badge";
+import CartCard from "../CartCard/CartCard";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
-function Navbar({ cartItems, setCartItems }) {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+function Navbar({ firstThreeSneakers , cartItems, setCartItems }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  //use location to disable cart button on /cart page
+  const location = useLocation();
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    return (
-        <AppBar style={{
-            backgroundColor: "#302f35"
-        }} position="static">
-            <Container maxWidth="x2">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'Inter',
-                            fontWeight: 700,
-                            letterSpacing: '.1rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        HeroSneakers
-                    </Typography>
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            <MenuItem onClick={handleCloseNavMenu}>
-                                <Typography
-                                    textAlign="center"
-                                    sx={{
-                                        fontFamily: 'Inter',
-                                    }}
-                                >
-                                    About Us
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem onClick={handleCloseNavMenu}>
-                                <Typography
-                                    textAlign="center"
-                                    sx={{ display: 'flex', fontFamily: 'Inter' }}
-                                >
-                                    Hot Deals
-                                    <LocalOfferIcon />
-                                </Typography>
-                            </MenuItem>
-                        </Menu>
-                    </Box>
+  return (
+    <AppBar
+      style={{
+        backgroundColor: "#302f35",
+      }}
+      position="static"
+    >
+      <Container maxWidth="x2">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Inter",
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            HeroSneakers
+          </Typography>
 
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Hero Sneakers
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        <Button
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'Inter' }}
-                        >
-                            About Us
-                        </Button>
-                        <Button
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'flex', fontFamily: 'Inter' }}
-                        >
-                            Hot Deals
-                            <LocalOfferIcon />
-                        </Button>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  textAlign="center"
+                  sx={{
+                    fontFamily: "Inter",
+                  }}
+                >
+                  About Us
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  textAlign="center"
+                  sx={{ display: "flex", fontFamily: "Inter" }}
+                >
+                  Hot Deals
+                  <LocalOfferIcon />
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
 
-                    </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Hero Sneakers
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: "white",
+                display: "block",
+                fontFamily: "Inter",
+              }}
+            >
+              About Us
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: "white",
+                display: "flex",
+                fontFamily: "Inter",
+              }}
+            >
+              Hot Deals
+              <LocalOfferIcon />
+            </Button>
+          </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Badge badgeContent={cartItems > 0 ? cartItems : null} color="primary">
-                            <IconButton sx={{ p: 0 }}>
-                                <Link to="/cart">
-                                    <ShoppingCart
-                                        style={{ color: 'white' }}
-                                    />
-                                </Link>
-                            </IconButton>
-                        </Badge>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          <Box sx={{ flexGrow: 0 }} className="menu-icon">
+            {location.pathname !== "/cart" && (
+              <div>
+                <Badge
+                  badgeContent={cartItems > 0 ? cartItems : null}
+                  color="primary"
+                  className="cart-icon"
+                >
+                  <IconButton>
+                    <Link to="/cart">
+                      <ShoppingCart style={{ color: "white" }} />
+                    </Link>
+                  </IconButton>
+                </Badge>
+                <div class="menu-list">
+                  <nav id="menu-nav">
+                    {firstThreeSneakers.map((sneaker, index) => (
+                        <CartCard key={sneaker.id} sneaker={sneaker} />
+                    ))}
+                  </nav>
+                </div>
+              </div>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 export default Navbar;

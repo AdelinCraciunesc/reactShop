c21432ac83399f1c9cebc2a868409a9e69a22a45
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2023-07-21 15:31:23 +0300

    Changed categories to allow links to added

--- sneakershop/src/components/Categories/Categories.jsx
+++ sneakershop/src/components/Categories/Categories.jsx
@@ -1,29 +1,32 @@
-import * as React from 'react';
-import { Grid } from '@mui/material';
-import CategoryCard from '../CategoryCard/CategoryCard';
-import "./Categories.css"
+import * as React from "react";
+import { Grid } from "@mui/material";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import "./Categories.css";
+import { Link } from "react-router-dom";
 
 export default function Categories() {
-
-  const cardInfo = [{
-    title: "Kids",
-    description: ["Check out the latest shoes for your kid"],
-    img: "https://www.rei.com/dam/van_dragt_092518_0150_kids_shoes_hero_lg.jpg?t=ea16by9lg"
-  },
-  {
-    title: "Men",
-    description: ["Never get out of fashion with this new drop"],
-    img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_800,w_800/enUS/Images/mens-shoes-v2-tcc_tcm221-909628.png"
-  },
-  {
-    title: "Women",
-    description: ["You'll just love those, trust me"],
-    img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_800,w_800/enUS/Images/womens-shoes-tcc_tcm221-909483.png"
-  }
-  ]
+  const cardInfo = [
+    {
+      title: "Kids",
+      description: ["Check out the latest shoes for your kid"],
+      img: "https://www.rei.com/dam/van_dragt_092518_0150_kids_shoes_hero_lg.jpg?t=ea16by9lg",
+    },
+    {
+      title: "Men",
+      description: ["Never get out of fashion with this new drop"],
+      img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_800,w_800/enUS/Images/mens-shoes-v2-tcc_tcm221-909628.png",
+    },
+    {
+      title: "Women",
+      description: ["You'll just love those, trust me"],
+      img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_800,w_800/enUS/Images/womens-shoes-tcc_tcm221-909483.png",
+    },
+  ];
 
   return (
-    <Grid container className='categories-div'
+    <Grid
+      container
+      className="categories-div"
       direction="row"
       justifyContent="center"
       alignItems="stretch"
@@ -31,8 +34,9 @@ export default function Categories() {
       sx={{
         mt: -2,
         borderRadius: 2,
-      }}>
-      {cardInfo.map((card, idx) => {
+      }}
+    >
+      {/* {cardInfo.map((card, idx) => {
         return (
           <Grid item>
             <CategoryCard
@@ -43,7 +47,30 @@ export default function Categories() {
             />
           </Grid>
         )
-      })}
+      })} */}
+      <Grid item>
+        <CategoryCard
+          title={cardInfo[0].title}
+          desc={cardInfo[0].description}
+          img={cardInfo[0].img}
+        />
+      </Grid>
+      <Grid item>
+        <Link to="/men">
+          <CategoryCard
+            title={cardInfo[1].title}
+            desc={cardInfo[1].description}
+            img={cardInfo[1].img}
+          />
+        </Link>
+      </Grid>
+      <Grid item>
+        <CategoryCard
+          title={cardInfo[2].title}
+          desc={cardInfo[2].description}
+          img={cardInfo[2].img}
+        />
+      </Grid>
     </Grid>
   );
 }
